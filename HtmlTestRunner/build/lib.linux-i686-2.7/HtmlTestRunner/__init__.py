0f3c8399e3724fbb74006d6bb959772f01d94691
Gun Kim <gune.kim@samsung.com>
2017-02-16 09:07:58 +0900

    MISC: remove unused file

--- HtmlTestRunner/build/lib.linux-i686-2.7/HtmlTestRunner/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from .runner import HTMLTestRunner
-import uiunittest
-
-
-__author__ = """Ordanis Sanchez Suero"""
-__email__ = 'ordanisanchez@gmail.com'
-__version__ = '1.0.3'
